3cbbf464e7f596edd1ffdf1f9ddc2762a4fa7275
Benjamin <bnjackson951@gmail.com>
2023-08-22 13:53:00 +0100

    Export tasksFile

--- utilities/file-handling.js
+++ utilities/file-handling.js
@@ -1,17 +1,21 @@
 'use sync';
 
 const fs = require('fs');
-const filePath = '../data/data.json';
+
+const filePath = './data/data.json';
 
 async function readTaskFile(filePath) {
     try {
-        const data = await fs.readfile(filePath);
+        const data = await fs.readFileSync(filePath);
         const parsedData = JSON.parse(data);
-        console.log(parsedData);
         return parsedData;
     } catch (err) {
         console.error(`Got an errror trying to read ${filePath} : ${err}`);
     }
 }
 
-readTaskFile(filePath);
+const tasksFile = readTaskFile(filePath);
+
+module.exports = {
+  tasksFile
+};
