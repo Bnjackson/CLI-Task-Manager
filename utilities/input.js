377e8dab294e352340a29c8d29760745e230c5ed
Benjamin <bnjackson951@gmail.com>
2023-08-23 12:47:26 +0100

    Add functionality to get users command choice input and export it

---
+++ utilities/input.js
@@ -0,0 +1,30 @@
+'use stict';
+
+const readlineSync = require('readline-sync');
+
+console.log(`The commands are:
+add - add a task,
+view - view all tasks,
+delete - delete an individual or all tasks,
+update - update an existing task
+`);
+
+async function getUserInput() {
+    const userInput = readlineSync.question('What command would you like to enter? ').toLowerCase();
+    function checkInput(userInput) {
+        const allowedInputs = ['add', 'view', 'delete', 'update'];
+        if (allowedInputs.includes(userInput)) {
+            return true;
+        } else {
+            console.log("Input must match one of these commands: 'add', 'view', 'delete', 'update'");
+            return false;
+        }
+    }
+    return checkInput(userInput) ? userInput : getUserInput();
+}
+
+const userCommand = getUserInput(); 
+
+module.exports = {
+    userCommand
+}
