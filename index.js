c7f85a6a6e7a01b789ced75a2d4d81e861321e33
Benjamin <bnjackson951@gmail.com>
2023-08-23 12:48:41 +0100

    Import file and import userCommand

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 'use strict';
 
 const fileHandlingModule = require('./utilities/file-handling.js');
+const inputModule = require('./utilities/input.js');
 
 async function main() {
+    console.log('This program is a task manager. It allows you add tasks to a tasks.json file. You can then view, update, or delete those tasks. All from your command line.');
     const tasksFile = await fileHandlingModule.tasksFile;
     console.log(tasksFile);
+    const userCommand = await inputModule.userCommand;
+    console.log(userCommand);
 }
 
 main();
