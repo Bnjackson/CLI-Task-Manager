b207ed04cc974d3aeea7e1f2348710acdc525d04
Benjamin <bnjackson951@gmail.com>
2023-08-25 13:04:26 +0100

    Call callCommandsFunctions asynchronously, only need to call writeTaskFile once instead of in each conditional block

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 'use strict';
+
 console.log('This program is a task manager. It allows you add tasks to a tasks.json file. You can then view, update, or delete those tasks. All from your command line.');
 
 //Imported modules
+const readlineSync = require('readline-sync');
 const fileHandlingModule = require('./utilities/file-handling.js');
 const inputModule = require('./utilities/input.js');
 const addModule = require('./commands/add.js');
@@ -13,21 +15,32 @@ async function main() {
     const tasksFile = await fileHandlingModule.tasksFile;
     const userCommand = await inputModule.userCommand;
 
-    function callCommandsFunctions(userCommand) {
+    async function callCommandsFunctions(userCommand) {
         if (userCommand === 'add') {
             addModule.addTask(tasksFile);
-            console.log(tasksFile.tasks);
         } else if (userCommand === 'delete') {
-
+            deleteModule.deleteTasks(tasksFile)
         } else if (userCommand === 'update') {
-
+            updateModule.updateTask(tasksFile);
         } else if (userCommand === 'view') {
             viewModule.outputTasks(tasksFile);
         } else {
             console.log(`ERROR: ${userCommand} does not match allowed commands`);
         }
+        await fileHandlingModule.writeTaskfile(tasksFile)
     }
-    callCommandsFunctions(userCommand);
+
+    await callCommandsFunctions(userCommand);
+
+    // function getAnotherCommand() {
+    //     const anotherCommand = readlineSync.question('Would you like to enter another command? Enter "y" for yes, and "n" for no: ').toLowerCase();
+    //     if (anotherCommand === 'y') {
+    //         main();
+    //     } else {
+    //         console.log('Thanks for using this program.');
+    //     }
+    // }
+    // getAnotherCommand();
 }
 
 main();
