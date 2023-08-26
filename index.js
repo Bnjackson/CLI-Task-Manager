54fb2d07bb88690e1142c2ad250523d1e300f3f3
Benjamin <bnjackson951@gmail.com>
2023-08-26 17:00:34 +0100

    Seperate nested functions from main functions, add try/catch to async calls, fix getAnotherCommandFunction

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 console.log('This program is a task manager. It allows you add tasks to a tasks.json file. You can then view, update, or delete those tasks. All from your command line.');
 
-//Imported modules
+//Imported required modules
 const readlineSync = require('readline-sync');
 const fileHandlingModule = require('./utilities/file-handling.js');
 const inputModule = require('./utilities/input.js');
@@ -11,11 +11,9 @@ const viewModule = require('./commands/view.js');
 const deleteModule = require('./commands/delete.js');
 const updateModule = require('./commands/update.js');
 
-async function main() {
-    const tasksFile = await fileHandlingModule.tasksFile;
-    const userCommand = await inputModule.userCommand;
-
-    async function callCommandsFunctions(userCommand) {
+//Function to handle user commands
+async function callCommandsFunctions(userCommand, tasksFile) {
+    try {
         if (userCommand === 'add') {
             addModule.addTask(tasksFile);
         } else if (userCommand === 'delete') {
@@ -28,19 +26,32 @@ async function main() {
             console.log(`ERROR: ${userCommand} does not match allowed commands`);
         }
         await fileHandlingModule.writeTaskfile(tasksFile)
+    } catch (error) {
+        console.error('An error occurred:', error.message);
     }
+}
 
-    await callCommandsFunctions(userCommand);
+async function main() {
+    try {
+        let shouldContinue = true;
+        while(shouldContinue) {
+            //Will run until getAnotherCommand returns false
+            console.log('Main called');
+            const tasksFile = await fileHandlingModule.tasksFile;
+            const userCommand = await inputModule.getUserInput();
+            await callCommandsFunctions(userCommand, tasksFile);
+            shouldContinue = shouldContinueLoop();
+        }
+        console.log('Thank you for using this program.')
+    } catch (error) {
+        console.error('An error occurred:', error.message);
+    }
+}
 
-    // function getAnotherCommand() {
-    //     const anotherCommand = readlineSync.question('Would you like to enter another command? Enter "y" for yes, and "n" for no: ').toLowerCase();
-    //     if (anotherCommand === 'y') {
-    //         main();
-    //     } else {
-    //         console.log('Thanks for using this program.');
-    //     }
-    // }
-    // getAnotherCommand();
+//Function to determine if the user wants to continue or end the loop
+function shouldContinueLoop () {
+    const userAnswer = readlineSync.question('Would you like to enter another command? Enter "y" or "n": ').toLowerCase();
+    return userAnswer === 'y';
 }
 
 main();
